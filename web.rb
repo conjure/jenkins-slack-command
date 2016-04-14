13db5f46c95934cc8bb0c911c9ba6e88034386b2
Ross Beale <rossbeale@gmail.com>
2016-04-14 11:09:34 +0100

    Update web.rb

--- web.rb
+++ web.rb
@@ -8,7 +8,6 @@ get '/' do
 end
 
 post '/' do
-
   # Verify all environment variables are set
   return [403, "No slack token setup"] unless slack_token = ENV['SLACK_TOKEN']
   return [403, "No jenkins url setup"] unless jenkins_url= ENV['JENKINS_URL']
@@ -18,42 +17,9 @@ post '/' do
   return [401, "No authorized for this command"] unless slack_token == params['token']
 
   # Split command text
-  text_parts = params['text'].split(' ')
-
-  # Split command text - job
-  job = text_parts[0]
-
-  # Split command text - parameters
-  parameters = []
-  if text_parts.size > 1
-    all_params = text_parts[1..-1]
-    all_params.each do |p|
-      p_thing = p.split('=')
-      parameters << { :name => p_thing[0], :value => p_thing[1] }
-    end
-  end
-
-  # Jenkins url
-  jenkins_job_url = "#{jenkins_url}/job/#{job}"
-
-  # Get next jenkins job build number
-  resp = RestClient.get "#{jenkins_job_url}/api/json"
-  resp_json = JSON.parse( resp.body )
-  next_build_number = resp_json['nextBuildNumber']
+  job = params['text']
+  build_url = "#{jenkins_url}/buildByToken/build?job=#{job}&token=#{jenkins_token}"
 
   # Make jenkins request
-  json = JSON.generate( {:parameter => parameters} )
-  resp = RestClient.post "#{jenkins_job_url}/build?token=#{jenkins_token}", :json => json
-
-  # Build url
-  build_url = "#{jenkins_job_url}/#{next_build_number}"
-
-  slack_webhook_url = ENV['SLACK_WEBHOOK_URL']
-  if slack_webhook_url
-    notifier = Slack::Notifier.new slack_webhook_url
-    notifier.ping "Started job '#{job}' - #{build_url}"
-  end
-
-  build_url
-
+  resp = RestClient.post URI.escape(build_url), {}
 end
